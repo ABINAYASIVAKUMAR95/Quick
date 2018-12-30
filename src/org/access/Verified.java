fb22ab141da9544ed99b59ef2a50611802e0449d
ABINAYASIVAKUMAR95 <sabinayakumar95@gmail.com>
2018-12-30 13:02:15 +0530

    Add method

---
+++ src/org/access/Verified.java
@@ -0,0 +1,16 @@
+package org.access;
+
+public class Verified {
+	
+	public static  void sum() {
+		int a=0,b=0;	
+		System.out.println(a+b);
+
+	}
+	public static void mul() {
+		int a=0,b=0;
+		System.out.println(a*b);
+
+	}
+
+}
